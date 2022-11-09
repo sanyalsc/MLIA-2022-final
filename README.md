f52e42a91df94b52c41f641da6ae686754c86c57
sanyalsc <sanyalster@gmail.com>
2022-11-08 21:52:10 -0800

    initial setup
    Add basic setup.py and directory structure, make basic README.md

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # MLIA-2022-final
 MLIA final project, fall 2022
+
+Shantanu Sanyal, Allan Wan, Isabelle Liu, Julian Nguyen
+
+SWIN UNETR for semantic segmentation of brain tumors implementation
+
+https://arxiv.org/pdf/2201.01266.pdf
+
+Installation:
+
+python -m pip -e <path to MLIA-2022-FINAL>
+
+Usage:
+TBD
+
---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(name='swin',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='https://github.com/sanyalsc/MLIA-2022-final',
+      author='MLIA Fall 2022 SWIN UNENT group',
+      author_email='sanyalster@gmail.com',
+      license='MIT',
+      packages=setuptools.find_packages("src"),
+      package_dir={'':"src"},
+      install_requires=[
+        'torch',
+        'pillow',
+      ])
